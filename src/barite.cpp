a6bcea6fb183b00011a01d1fa6534d8361c1cb3a
Jakob Bentzon <ext-jakb@ecco.com>
2022-01-29 22:41:52 +0100

    Simplify

--- src/barite.cpp
+++ src/barite.cpp
@@ -41,18 +41,6 @@ const BariteReaction getBaSO4Reaction()
     return BariteReaction(nu_A, nu_B, nu_P, Z_A, Z_B);
 }
 
-void DebyeHuckelActivity(Real *result, int size, Real *Temperature, Real *mA, Real *mB, Real *yEtc_1, Real *yEtc_2)
-{
-    BariteReaction React = getBaSO4Reaction();
-    React.DebyeHuckelActivity(result, size, Temperature, yEtc_1, yEtc_2);
-}
-
-void DebyeHuckelSaturationIndex(Real *result, int size, Real *Temperature, Real *yA, Real *yB, Real *yEtc_1, Real *yEtc_2)
-{
-    BariteReaction React = getBaSO4Reaction();
-    React.DebyeHuckelSaturationIndex(result, size, Temperature, yA, yB, yEtc_1, yEtc_2);
-}
-
 void EquilibriumConstant(Real *result, int size, Real *Temperature, Real *yA, Real *yB, Real *yEtc_1, Real *yEtc_2)
 {
     BariteReaction React = getBaSO4Reaction();
@@ -82,104 +70,10 @@ void MeanMolality(Real *result, int size, Real *yA, Real *yB, Real *yEtc_1, Real
     React.MeanMolality(result, size, yA, yB, yEtc_1, yEtc_2);
 }
 
-void ExtendedDebyeHuckelNucleationRate(Real *result, int size, Real *Temperature, Real *yA, Real *yB, Real *yEtc_1, Real *yEtc_2)
-{
-    BariteReaction React = getBaSO4Reaction();
-    const Real (NucleationRate::*SatFunctionPointer)(Real, Real, Real, Real, Real) = &BariteReaction::ExtendedDebyeHuckelSaturationIndex;
-    React.NucleationRate(result, size, Temperature, yA, yB, yEtc_1, yEtc_2, &React, SatFunctionPointer);
-}
-
-void ExtendedDebyeHuckelWallReactionRateMolality(Real *result, int size, Real *Temperature, Real *yA, Real *yB, Real *yEtc_1, Real *yEtc_2, Real *SaturationIndex, Real *WallDistance)
-{
-    BariteReaction React = getBaSO4Reaction();
-    React.WallReactionRateMolality(result, size, Temperature, yA, yB, yEtc_1, yEtc_2, SaturationIndex, WallDistance);
-}
-
-void ExtendedDebyeHuckelWallReactionRateMoleFraction(Real *result, int size, Real *Temperature, Real *yA, Real *yB, Real *yEtc_1, Real *yEtc_2, Real *SaturationIndex, Real *WallDistance)
-{
-    BariteReaction React = getBaSO4Reaction();
-    React.WallReactionRateMoleFraction(result, size, Temperature, yA, yB, yEtc_1, yEtc_2, SaturationIndex, WallDistance);
-}
-
-void ExtendedDebyeHuckelWallReactionRateDerivativeAMoleFraction(Real *result, int size, Real *Temperature, Real *yA, Real *yB, Real *yEtc_1, Real *yEtc_2, Real *SaturationIndex, Real *WallDistance)
-{
-    BariteReaction React = getBaSO4Reaction();
-    React.DebyeHuckelWallReactionDerivativeA(result, size, Temperature, yA, yB, yEtc_1, yEtc_2, SaturationIndex, WallDistance);
-}
-
-void ExtendedDebyeHuckelWallReactionRateDerivativeBMoleFraction(Real *result, int size, Real *Temperature, Real *yA, Real *yB, Real *yEtc_1, Real *yEtc_2, Real *SaturationIndex, Real *WallDistance)
-{
-    BariteReaction React = getBaSO4Reaction();
-    React.DebyeHuckelWallReactionDerivativeB(result, size, Temperature, yA, yB, yEtc_1, yEtc_2, SaturationIndex, WallDistance);
-}
-
-void WallConcentrationA(Real *result, int size, Real *Temperature, Real *yA, Real *yB, Real *yEtc_1, Real *yEtc_2)
-{
-    BariteReaction React = getBaSO4Reaction();
-    React.PitzerWallConcentrationA(result, size, Temperature, yA, yB, yEtc_1, yEtc_2);
-}
-
-void WallConcentrationB(Real *result, int size, Real *Temperature, Real *yA, Real *yB, Real *yEtc_1, Real *yEtc_2)
-{
-    BariteReaction React = getBaSO4Reaction();
-    React.PitzerWallConcentrationB(result, size, Temperature, yA, yB, yEtc_1, yEtc_2);
-}
-
 void uclib()
 {
-
     ucfunc((void *)EquilibriumConstant, "ScalarFieldFunction", "Equilibrium Constant");
     ucarg((void *)EquilibriumConstant, "Cell", "Temperature", sizeof(Real));
-
-    ucfunc((void *)DebyeHuckelActivity, "ScalarFieldFunction", "Debye Huckel Activity Coefficient");
-    ucarg((void *)DebyeHuckelActivity, "Cell", "Temperature", sizeof(Real));
-    ucarg((void *)DebyeHuckelActivity, "Cell", "$yBa_2+", sizeof(double));
-    ucarg((void *)DebyeHuckelActivity, "Cell", "$ySO4_2-", sizeof(Real));
-    ucarg((void *)DebyeHuckelActivity, "Cell", "$yEtc_1-", sizeof(Real));
-    ucarg((void *)DebyeHuckelActivity, "Cell", "$yEtc_2-", sizeof(Real));
-
-    ucfunc((void *)DebyeHuckelSaturationIndex, "ScalarFieldFunction", "Debye Huckel Saturation Index");
-    ucarg((void *)DebyeHuckelSaturationIndex, "Cell", "Temperature", sizeof(Real));
-    ucarg((void *)DebyeHuckelSaturationIndex, "Cell", "$yBa_2+", sizeof(Real));
-    ucarg((void *)DebyeHuckelSaturationIndex, "Cell", "$ySO4_2-", sizeof(Real));
-    ucarg((void *)DebyeHuckelSaturationIndex, "Cell", "$yEtc_1-", sizeof(Real));
-    ucarg((void *)DebyeHuckelSaturationIndex, "Cell", "$yEtc_2-", sizeof(Real));
-
-    ucfunc((void *)ExtendedDebyeHuckelWallReactionRateMolality, "ScalarFieldFunction", "Debye Huckel Wall Deposition (Molality)");
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateMolality, "Cell", "Temperature", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateMolality, "Cell", "$yBa_2+", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateMolality, "Cell", "$ySO4_2-", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateMolality, "Cell", "$yEtc_1-", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateMolality, "Cell", "$yEtc_2-", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateMolality, "Cell", "$UserDebyeHuckelSaturationIndex", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateMolality, "Cell", "$WallDistance", sizeof(Real));
-
-    ucfunc((void *)ExtendedDebyeHuckelWallReactionRateMoleFraction, "ScalarFieldFunction", "Debye Huckel Wall Deposition (Mole Fraction)");
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateMoleFraction, "Cell", "Temperature", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateMoleFraction, "Cell", "$yBa_2+", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateMoleFraction, "Cell", "$ySO4_2-", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateMoleFraction, "Cell", "$yEtc_1-", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateMoleFraction, "Cell", "$yEtc_2-", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateMoleFraction, "Cell", "$UserDebyeHuckelSaturationIndex", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateMoleFraction, "Cell", "$WallDistance", sizeof(Real));
-
-    ucfunc((void *)ExtendedDebyeHuckelWallReactionRateDerivativeAMoleFraction, "ScalarFieldFunction", "Debye Huckel Wall Deposition Derivative Ba (Mole Fraction)");
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateDerivativeAMoleFraction, "Cell", "Temperature", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateDerivativeAMoleFraction, "Cell", "$yBa_2+", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateDerivativeAMoleFraction, "Cell", "$ySO4_2-", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateDerivativeAMoleFraction, "Cell", "$yEtc_1-", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateDerivativeAMoleFraction, "Cell", "$yEtc_2-", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateDerivativeAMoleFraction, "Cell", "$UserDebyeHuckelSaturationIndex", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateDerivativeAMoleFraction, "Cell", "$WallDistance", sizeof(Real));
-
-    ucfunc((void *)ExtendedDebyeHuckelWallReactionRateDerivativeBMoleFraction, "ScalarFieldFunction", "Debye Huckel Wall Deposition Derivative SO4 (Mole Fraction)");
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateDerivativeBMoleFraction, "Cell", "Temperature", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateDerivativeBMoleFraction, "Cell", "$yBa_2+", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateDerivativeBMoleFraction, "Cell", "$ySO4_2-", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateDerivativeBMoleFraction, "Cell", "$yEtc_1-", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateDerivativeBMoleFraction, "Cell", "$yEtc_2-", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateDerivativeBMoleFraction, "Cell", "$UserDebyeHuckelSaturationIndex", sizeof(Real));
-    ucarg((void *)ExtendedDebyeHuckelWallReactionRateDerivativeBMoleFraction, "Cell", "$WallDistance", sizeof(Real));
 
     ucfunc((void *)IonicStrength, "ScalarFieldFunction", "Ionic Strength");
     ucarg((void *)IonicStrength, "Cell", "$yEtc_1-", sizeof(Real));
@@ -204,18 +98,4 @@ void uclib()
     ucarg((void *)PitzerSaturationIndex, "Cell", "$ySO4_2-", sizeof(Real));
     ucarg((void *)PitzerSaturationIndex, "Cell", "$yEtc_1-", sizeof(Real));
     ucarg((void *)PitzerSaturationIndex, "Cell", "$yEtc_2-", sizeof(Real));
-
-    ucfunc((void *)WallConcentrationA, "ScalarFieldFunction", "Pitzer Diff Model Wall mBa_2+");
-    ucarg((void *)WallConcentrationA, "Cell", "Temperature", sizeof(Real));
-    ucarg((void *)WallConcentrationA, "Cell", "$yBa_2+", sizeof(Real));
-    ucarg((void *)WallConcentrationA, "Cell", "$ySO4_2-", sizeof(Real));
-    ucarg((void *)WallConcentrationA, "Cell", "$yEtc_1-", sizeof(Real));
-    ucarg((void *)WallConcentrationA, "Cell", "$yEtc_2-", sizeof(Real));
-
-    ucfunc((void *)WallConcentrationB, "ScalarFieldFunction", "Pitzer Diff Model Wall mSO4_2-");
-    ucarg((void *)WallConcentrationB, "Cell", "Temperature", sizeof(Real));
-    ucarg((void *)WallConcentrationB, "Cell", "$yBa_2+", sizeof(Real));
-    ucarg((void *)WallConcentrationB, "Cell", "$ySO4_2-", sizeof(Real));
-    ucarg((void *)WallConcentrationB, "Cell", "$yEtc_1-", sizeof(Real));
-    ucarg((void *)WallConcentrationB, "Cell", "$yEtc_2-", sizeof(Real));
 }
