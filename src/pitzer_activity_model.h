21f1c82afdfdedf418d2052c4cefdfe57b4cab05
Jakob Bentzon <ext-jakb@ecco.com>
2022-01-30 21:07:33 +0100

    Test

--- src/pitzer_activity_model.h
+++ src/pitzer_activity_model.h
@@ -93,7 +93,7 @@ public:
         const Real A = DebyeHuckelParam(T); //kg/mol
         const Real B_gamma = 2 * beta_0 + 2 * beta_1 / ((alpha_1 * alpha_1) * I) * (1 - (1 + alpha_1 * sqrt(I) - 0.5 * (alpha_1 * alpha_1) * I) * exp(-alpha_1 * sqrt(I))) + 2 * beta_2 / ((alpha_2 * alpha_2) * I) * (1 - (1 + alpha_2 * sqrt(I) - 0.5 * (alpha_2 * alpha_2) * I) * exp(-alpha_2 * sqrt(I)));
 
-        reurn B_gamma;
+        return B_gamma;
         const Real f_gamma = -A / 3 * (sqrt(I) / (1 + b * sqrt(I)) + 2 / b * log(1 + b * sqrt(I)));
 
         const Real C_gamma = 3 / 2 * C_Phi;
