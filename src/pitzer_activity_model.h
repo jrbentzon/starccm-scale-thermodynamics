1fb4524832ee8bd611c173ceea2b73c3699079cd
Jakob Bentzon <ext-jakb@ecco.com>
2022-01-30 18:11:04 +0100

    test

--- src/pitzer_activity_model.h
+++ src/pitzer_activity_model.h
@@ -61,7 +61,8 @@ public:
 
     Real getActivityCoefficient(Real T, Real yA, Real yB, Real yEtc1, Real yEtc2)
     {
-        return pitzerActivityCoefficient(T, IonicStrength(yEtc1, yEtc2), reaction.MeanMolality(yA, yA, yEtc1, yEtc2));
+        return IonicStrength(yEtc1, yEtc2);
+        //return pitzerActivityCoefficient(T, IonicStrength(yEtc1, yEtc2), reaction.MeanMolality(yA, yA, yEtc1, yEtc2));
     };
 
     // Activity coefficient from Pitzer's eq.
